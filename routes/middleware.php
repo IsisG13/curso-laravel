b43592e7691ca4896294f8492fc5420f88e2e321
IsisG13 <isissouzaguimaraes@gmail.com>
2022-06-28 15:39:31 -0300

    middleware

---
+++ routes/middleware.php
@@ -0,0 +1,25 @@
+
+<!-- Route::get('/', function(){
+    return view('welcome');
+});
+
+// Middleware global
+
+Route::get('admin', function(){
+    dd('admin');
+})->middleware('checkToken:editor');
+
+// Route::middleware(['myApp'])->group( function(){
+
+//     Route::get('users', function(){
+//         dd('users');
+//     });
+
+//     Route::get('posts', function(){
+//         dd('posts');
+//     });
+
+//     Route::get('services', function(){
+//         dd('services');
+//     })->withoutMiddleware('userAgent');
+// }); -->
